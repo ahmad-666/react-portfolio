d78bf7578baa43ff5a9bde870f796d0e3cb038f7
ahmad-666 <ahmadrezagghz666@gmail.com>
2021-01-21 18:56:08 +0330

    full deploy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 // import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
